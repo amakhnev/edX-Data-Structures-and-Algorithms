14922c144732b14671de141d4743914f2c55eace
Alexey Makhnev <alexey@integrace.co.uk>
2018-06-23 11:28:08 +0100

    fibonacci numbers solution

--- 01-Algorithmic-Design-and-Techniques/week2_algorithmic_warmup/1_fibonacci_number/Fibonacci.java
+++ 01-Algorithmic-Design-and-Techniques/week2_algorithmic_warmup/1_fibonacci_number/Fibonacci.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
 
 public class Fibonacci {
-  private static long calc_fib(int n) {
+  private static long calc_fib_rec(int n) {
     if (n <= 1)
       return n;
 
-    return calc_fib(n - 1) + calc_fib(n - 2);
+    return calc_fib_rec(n - 1) + calc_fib_rec(n - 2);
+  }
+
+
+  private static long calc_fib(int n) {
+      if (n <= 1)
+          return n;
+
+      long first = 1;
+      long second =1;
+
+      for (int i=3;i<=n;i++){
+          long next = first+second;
+          first = second;
+          second = next;
+      }
+      return second;
   }
 
   public static void main(String args[]) {
---
+++ 01-Algorithmic-Design-and-Techniques/week2_algorithmic_warmup/readme.md
@@ -0,0 +1,7 @@
+Grading reports:
+___
+[Executed at: Sat Jun 23 3:23:51 PDT 2018]
+
+Fibonacci:
+Good job! (Max time used: 0.08/1.50, max memory used: 28712960/536870912.)
+___
