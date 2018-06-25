4ab374af1056427a4bdeb3bfcf32e01293f40dcd
Alexey Makhnev <alexey@integrace.co.uk>
2018-06-25 17:07:18 +0100

    implementation

--- 01-Algorithmic-Design-and-Techniques/week3_greedy_algorithms/1_money_change/Change.java
+++ 01-Algorithmic-Design-and-Techniques/week3_greedy_algorithms/1_money_change/Change.java
@@ -3,7 +3,20 @@ import java.util.Scanner;
 public class Change {
     private static int getChange(int m) {
         //write your code here
-        return m;
+        int q = m;
+        int result = 0;
+        if (q >= 10) {
+            result += q / 10;
+            q = q % 10;
+        }
+
+        if (q >= 5) {
+            result += q / 5;
+            q = q % 5;
+        }
+        result += q;
+
+        return result;
     }
 
     public static void main(String[] args) {
--- 01-Algorithmic-Design-and-Techniques/week3_greedy_algorithms/2_maximum_value_of_the_loot/FractionalKnapsack.java
+++ 01-Algorithmic-Design-and-Techniques/week3_greedy_algorithms/2_maximum_value_of_the_loot/FractionalKnapsack.java
@@ -1,11 +1,35 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class FractionalKnapsack {
     private static double getOptimalValue(int capacity, int[] values, int[] weights) {
-        double value = 0;
-        //write your code here
 
-        return value;
+        Item[] items = new Item[values.length];
+        for (int i = 0; i < values.length; i++) {
+            items[i] = new Item(values[i], weights[i]);
+        }
+
+        Arrays.sort(items, new Comparator<Item>() {
+            @Override
+            public int compare(Item p1, Item p2) {
+                if (p1.price > p2.price) return -1;
+                if (p1.price < p2.price) return 1;
+                return 0;
+            }
+        });
+
+        int result = 0, capacityLeft = capacity;
+
+        for (int i = 0; i < items.length && capacityLeft > 0; i++) {
+            if (capacityLeft > items[i].weight) {
+                result += items[i].value;
+                capacityLeft -= items[i].weight;
+            } else {
+                result += items[i].price * capacityLeft;
+                capacityLeft = 0;
+            }
+        }
+
+        return result;
     }
 
     public static void main(String args[]) {
@@ -19,5 +43,16 @@ public class FractionalKnapsack {
             weights[i] = scanner.nextInt();
         }
         System.out.println(getOptimalValue(capacity, values, weights));
+    }
+
+    private static class Item {
+        int value, weight;
+        double price;
+
+        public Item(int value, int weight) {
+            this.value = value;
+            this.weight = weight;
+            this.price = (double) value / weight;
+        }
     }
 } 
