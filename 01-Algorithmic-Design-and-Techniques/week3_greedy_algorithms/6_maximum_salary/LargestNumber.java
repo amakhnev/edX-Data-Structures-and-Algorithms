f8e06f291f0c8b8d57b1904d5e1859d65ed711c5
Alexey Makhnev <alexey@integrace.co.uk>
2018-07-01 12:59:54 +0100

    correct comparator implementation

--- 01-Algorithmic-Design-and-Techniques/week3_greedy_algorithms/6_maximum_salary/LargestNumber.java
+++ 01-Algorithmic-Design-and-Techniques/week3_greedy_algorithms/6_maximum_salary/LargestNumber.java
@@ -2,21 +2,10 @@ import java.util.*;
 
 public class LargestNumber {
     private static String largestNumber(String[] a) {
-        Arrays.sort(a, new Comparator<String>() { // ("9"<"8") and ("92" < "928")
+        Arrays.sort(a, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                if (o1.length() == 0 || o2.length() == 0) {
-                    if (o1.length() > 0) return 1;
-                    if (o2.length() > 0) return -1;
-                    return 0;
-                }
-                if (o1.charAt(0) == o2.charAt(0)) {
-                    return this.compare(o1.substring(1), o2.substring(1));
-                } else {
-                    if (o1.charAt(0) > o2.charAt(0)) return -1;
-                    if (o1.charAt(0) < o2.charAt(0)) return 1;
-                }
-                return 0;
+                return (o2 + o1).compareTo(o1 + o2);
             }
         });
 
