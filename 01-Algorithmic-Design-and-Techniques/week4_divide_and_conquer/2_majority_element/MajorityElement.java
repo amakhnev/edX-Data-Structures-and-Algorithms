8ed9fefb9efdf57fb938bc2e6c4dcb28ef4f3f50
Alexey Makhnev <alexey@integrace.co.uk>
2018-07-02 11:02:41 +0100

    proper divide and conquer implementation

--- 01-Algorithmic-Design-and-Techniques/week4_divide_and_conquer/2_majority_element/MajorityElement.java
+++ 01-Algorithmic-Design-and-Techniques/week4_divide_and_conquer/2_majority_element/MajorityElement.java
@@ -2,24 +2,42 @@ import java.util.*;
 import java.io.*;
 
 public class MajorityElement {
-    private static int getMajorityElement(int[] a) {
+    private static int getMajorityElement(int[] a, int left, int right) {
 
-        HashMap<Integer,Integer> counter = new HashMap<>();
-        for (int i = 0;i<a.length;i++){
-            if (counter.containsKey(a[i])){
-                counter.replace(a[i],counter.get(a[i])+1);
-            }else{
-                counter.put(a[i],1);
+        if (left == right) {
+            return -1;
+        }
+        if (left + 1 == right) {
+            return a[left];
+        }
+        
+        int leftMax = getMajorityElement(a,left,(right+left)/2);
+        if (leftMax>0){
+            int counter =0;
+            for (int i=0;i<a.length;i++){
+                if (a[i]==leftMax){
+                    counter++;
+                    if (counter>a.length/2){
+                        return leftMax;
+                    }
+                }
+            }
+        }
+        int rightMax = getMajorityElement(a,(right+left)/2,right);
+        if (rightMax>0){
+            int counter =0;
+            for (int i=a.length-1;i>=0;i--){
+                if (a[i]==rightMax){
+                    counter++;
+                    if (counter>a.length/2){
+                        return rightMax;
+                    }
+                }
             }
         }
 
-        for (int key :
-                counter.keySet()) {
-            if (counter.get(key)>a.length/2){
-                return key;
-            }
-        }
         return -1;
+
     }
 
     public static void main(String[] args) throws Exception {
@@ -31,7 +49,7 @@ public class MajorityElement {
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-        int max = getMajorityElement(a);
+        int max = getMajorityElement(a,0, a.length);
         if (max != -1) {
             System.out.println(max);
         } else {
