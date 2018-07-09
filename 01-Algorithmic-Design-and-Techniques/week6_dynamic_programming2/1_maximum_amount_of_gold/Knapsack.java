e3c4c6673c004fac968c63ff8fad55e6e307c5cb
Alexey Makhnev <alexey@integrace.co.uk>
2018-07-09 14:38:35 +0100

    w6 solutions

--- 01-Algorithmic-Design-and-Techniques/week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
+++ 01-Algorithmic-Design-and-Techniques/week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
@@ -1,19 +1,28 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Knapsack {
     static int optimalWeight(int W, int[] w) {
-        //write you code here
-        int result = 0;
-        for (int i = 0; i < w.length; i++) {
-          if (result + w[i] <= W) {
-            result += w[i];
-          }
+
+        int [][] values = new int[w.length+1][W+1];
+
+        for (int i=1;i<=w.length;i++){
+            for (int j=1;j<=W;j++){
+                if (j-w[i-1]>=0){
+                    values[i][j] = Math.max(values[i-1][j],values[i-1][j-w[i-1]]+w[i-1]);
+                }else{
+                    values[i][j] = values[i-1][j];
+                }
+
+            }
         }
-        return result;
+        return values[w.length][W];
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new FileInputStream("C:\\Users\\User\\Downloads\\temp\\6_1_knapsack.in"));
         int W, n;
         W = scanner.nextInt();
         n = scanner.nextInt();
